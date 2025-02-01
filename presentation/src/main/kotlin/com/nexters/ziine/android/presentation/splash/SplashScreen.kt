9fb5edc825de928c4ce0ebd2e3928e68ef42250a
easyhooon <mraz3068@gmail.com>
2025-02-02 02:17:25 +0900

    [feat] 임시 스플래시 이미지 추가 (#4)
    로띠 파일 전달받으면 로띠로 교체 예정

--- presentation/src/main/kotlin/com/nexters/ziine/android/presentation/splash/SplashScreen.kt
+++ presentation/src/main/kotlin/com/nexters/ziine/android/presentation/splash/SplashScreen.kt
@@ -1,17 +1,15 @@
 package com.nexters.ziine.android.presentation.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import com.nexters.ziine.android.presentation.R
-import com.nexters.ziine.android.presentation.ui.theme.Subtitle1
 
 @Composable
 fun SplashScreen(modifier: Modifier = Modifier) {
@@ -23,10 +21,9 @@ fun SplashScreen(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center,
         ) {
-            Text(
-                text = stringResource(R.string.ziine),
-                style = Subtitle1,
-                color = Color.Black
+            Image(
+                painter = painterResource(R.drawable.ziine_splash),
+                contentDescription = "Ziine Splash",
             )
         }
     }
