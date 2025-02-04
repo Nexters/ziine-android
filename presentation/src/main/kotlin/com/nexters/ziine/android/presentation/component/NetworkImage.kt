2f45c9d9c4a1730ed728bcbf5874a52c2a092666
easyhooon <jihun.lee@medisolution.co.kr>
2025-02-04 11:58:47 +0900

    [fix] 작품목록 화면 위로 스크롤시 스크롤 영역 skip, bounce 되는 문제 해결 (#15)
    전역적으로 설정한 이미지 캐싱 전략과 NetworkImage 내에 ImageLoader 의 캐싱 전략이 충돌되어 발생했다고 판단되어짐

--- presentation/src/main/kotlin/com/nexters/ziine/android/presentation/component/NetworkImage.kt
+++ presentation/src/main/kotlin/com/nexters/ziine/android/presentation/component/NetworkImage.kt
@@ -2,24 +2,15 @@ package com.nexters.ziine.android.presentation.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
-import coil.ImageLoader
-import coil.memory.MemoryCache
-import coil.request.ImageRequest
+import coil.compose.AsyncImage
 import com.nexters.ziine.android.presentation.R
 import com.nexters.ziine.android.presentation.preview.ComponentPreview
 import com.nexters.ziine.android.presentation.ui.theme.ZiineTheme
-import com.skydoves.landscapist.ImageOptions
-import com.skydoves.landscapist.animation.crossfade.CrossfadePlugin
-import com.skydoves.landscapist.coil.CoilImage
-import com.skydoves.landscapist.components.rememberImageComponent
-import com.skydoves.landscapist.placeholder.placeholder.PlaceholderPlugin
 
 @Composable
 fun NetworkImage(
@@ -27,43 +18,11 @@ fun NetworkImage(
     contentDescription: String?,
     modifier: Modifier = Modifier,
     loadingImage: Painter = painterResource(id = R.drawable.placeholder),
-    failureImage: Painter = painterResource(id = R.drawable.placeholder),
+    // failureImage: Painter = painterResource(id = R.drawable.placeholder),
     contentScale: ContentScale = ContentScale.Crop,
 ) {
-    val context = LocalContext.current
-
-    if (LocalInspectionMode.current) {
-        Image(
-            painter = loadingImage,
-            contentDescription = "Example Image Icon",
-            modifier = modifier,
-        )
-    } else {
-        CoilImage(
-            imageRequest = {
-                ImageRequest.Builder(context)
-                    .data(imageUrl)
-                    .build()
-            },
-            imageLoader = {
-                ImageLoader.Builder(context)
-                    .memoryCache { MemoryCache.Builder(context).maxSizePercent(0.25).build() }
-                    .build()
-            },
-            modifier = modifier,
-            component = rememberImageComponent {
-                +CrossfadePlugin(duration = 500)
-                // 사진이 어떻게 나오는지 확인하기 위해 개발용으로 넣어둠, API 연동 후 제거 예정
-                // +PlaceholderPlugin.Loading(loadingImage)
-                +PlaceholderPlugin.Failure(failureImage)
-            },
-            imageOptions = ImageOptions(
-                contentScale = contentScale,
-                alignment = Alignment.Center,
-                contentDescription = contentDescription,
-            ),
-        )
-    }
+//    val context = LocalContext.current
+//
 //    if (LocalInspectionMode.current) {
 //        Image(
 //            painter = loadingImage,
@@ -71,23 +30,42 @@ fun NetworkImage(
 //            modifier = modifier,
 //        )
 //    } else {
-//        val imageLoader = ImageLoader.Builder(context)
-//            .memoryCache {
-//                MemoryCache.Builder(context)
-//                    .maxSizePercent(0.25)
-//                    .build()
-//            }
-//            .build()
-//
-//        AsyncImage(
-//            model = imageUrl,
-//            contentDescription = contentDescription,
+//        CoilImage(
+//            imageModel = { imageUrl },
 //            modifier = modifier,
-//            contentScale = contentScale,
-//            placeholder = loadingImage,
-//            imageLoader = imageLoader
+//            component = rememberImageComponent {
+//                +CrossfadePlugin(duration = 500)
+//                // 사진이 어떻게 나오는지 확인하기 위해 개발용으로 넣어둠, API 연동 후 제거 예정
+//                // +PlaceholderPlugin.Loading(loadingImage)
+//                +PlaceholderPlugin.Failure(failureImage)
+//            },
+//            imageOptions = ImageOptions(
+//                contentScale = contentScale,
+//                alignment = Alignment.Center,
+//                contentDescription = contentDescription,
+//            ),
 //        )
 //    }
+    if (LocalInspectionMode.current) {
+        Image(
+            painter = loadingImage,
+            contentDescription = "Example Image Icon",
+            modifier = modifier,
+        )
+    } else {
+        AsyncImage(
+//            model = ImageRequest.Builder(LocalContext.current)
+//                .data(imageUrl)
+//                .crossfade(true)
+//                .build(),
+            model = imageUrl,
+            contentDescription = contentDescription,
+            modifier = modifier,
+            contentScale = contentScale,
+            // 사진이 어떻게 나오는지 확인하기 위해 개발용으로 넣어둠, API 연동 후 제거 예정
+            placeholder = loadingImage,
+        )
+    }
 }
 
 @ComponentPreview
