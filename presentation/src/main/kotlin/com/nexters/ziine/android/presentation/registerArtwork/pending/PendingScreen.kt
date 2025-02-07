3fd3add6d9a1f51bf1a587da320559b2331fdbbf
changhwan <chang5hustle@gmail.com>
2025-02-08 01:53:47 +0900

    [feat] 하단 버튼 구성 (#23)

--- presentation/src/main/kotlin/com/nexters/ziine/android/presentation/registerArtwork/pending/PendingScreen.kt
+++ presentation/src/main/kotlin/com/nexters/ziine/android/presentation/registerArtwork/pending/PendingScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -13,7 +14,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,6 +42,7 @@ import com.nexters.ziine.android.presentation.registerArtwork.pending.model.UiPe
 import com.nexters.ziine.android.presentation.ui.theme.Heading4
 import com.nexters.ziine.android.presentation.ui.theme.Paragraph3
 import com.nexters.ziine.android.presentation.ui.theme.Subtitle1
+import com.nexters.ziine.android.presentation.ui.theme.Subtitle2
 import com.nexters.ziine.android.presentation.ui.theme.ZiineTheme
 import java.util.Locale
 
@@ -45,7 +50,7 @@ import java.util.Locale
 internal fun PendingRoute(modifier: Modifier = Modifier, activityFinishAction: () -> Unit) {
     PendingScreen(
         modifier = modifier,
-        activityFinishAction
+        activityFinishAction,
     )
 }
 
@@ -57,19 +62,20 @@ internal fun PendingScreen(modifier: Modifier = Modifier, activityFinishAction: 
     LaunchedEffect(scrollState.value) { isScrolled = scrollState.value > 0 }
 
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
     ) {
         RegisterTopBar(isScrolled = isScrolled) { activityFinishAction() }
-        GuideUI(scrollState)
+        GuideUI(scrollState, Modifier.weight(1f))
+        StickyFooter()
     }
 }
 
 @Composable
-private fun GuideUI(scrollState: ScrollState) {
+private fun GuideUI(scrollState: ScrollState, modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
-            .verticalScroll(scrollState)
+            .verticalScroll(scrollState),
     ) {
         GuideMainTitle()
         GuideContent(getPendingGuideItems())
@@ -83,19 +89,19 @@ private fun GuideMainTitle() {
             .fillMaxWidth()
             .height(122.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
             text = stringResource(R.string.register_artwork_main_title_first),
             color = MaterialTheme.colorScheme.onBackground,
-            style = Heading4
+            style = Heading4,
 
-        )
+            )
         Spacer(modifier = Modifier.height(12.dp))
         Text(
             text = stringResource(R.string.register_artwork_main_title_second),
             color = MaterialTheme.colorScheme.onBackground,
-            style = Heading4
+            style = Heading4,
         )
     }
 }
@@ -129,7 +135,7 @@ private fun getPendingGuideItems(): List<UiPendingGuideItem> =
                 }
                 append(stringResource(R.string.register_artwork_first_content_content_second))
             },
-            imageId = R.drawable.ic_arrow_back
+            imageId = R.drawable.ic_arrow_back,
         ),
         UiPendingGuideItem(
             title = stringResource(R.string.register_artwork_second_content_title),
@@ -143,7 +149,7 @@ private fun getPendingGuideItems(): List<UiPendingGuideItem> =
                 }
                 append(stringResource(R.string.register_artwork_second_content_content_fourth))
             },
-            imageId = R.drawable.ic_arrow_back
+            imageId = R.drawable.ic_arrow_back,
         ),
         UiPendingGuideItem(
             title = stringResource(R.string.register_artwork_third_content_title),
@@ -152,8 +158,8 @@ private fun getPendingGuideItems(): List<UiPendingGuideItem> =
                     append(stringResource(R.string.register_artwork_thirdcontent_content_first))
                 }
             },
-            imageId = R.drawable.ic_arrow_back
-        )
+            imageId = R.drawable.ic_arrow_back,
+        ),
     )
 
 @Composable
@@ -161,30 +167,30 @@ private fun GuideContentGeneralForm(
     order: Int,
     title: String,
     content: AnnotatedString,
-    @DrawableRes imageId: Int
+    @DrawableRes imageId: Int,
 ) {
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp)
+            .padding(horizontal = 16.dp),
     ) {
         Text(
             text = String.format(Locale.KOREA, "%02d", order),
             color = MaterialTheme.colorScheme.onBackground,
-            style = Subtitle1
+            style = Subtitle1,
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = title,
             color = MaterialTheme.colorScheme.onBackground,
-            style = Subtitle1
+            style = Subtitle1,
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = content,
             color = MaterialTheme.colorScheme.onBackground,
-            style = Paragraph3
+            style = Paragraph3,
         )
         Spacer(modifier = Modifier.height(14.dp))
         Image(
@@ -193,8 +199,36 @@ private fun GuideContentGeneralForm(
                 .padding(horizontal = 11.5.dp)
                 .aspectRatio(320f / 240f),
             painter = painterResource(id = imageId),
-            contentDescription = null
+            contentDescription = null,
         )
+    }
+}
+
+@Composable
+private fun StickyFooter() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.background),
+    ) {
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(6.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+            ),
+        ) {
+            Text(
+                text = stringResource(R.string.register_artwork_footer_button_text),
+                style = Subtitle2,
+            )
+        }
+        Spacer(modifier = Modifier.height(40.dp))
     }
 }
 
@@ -204,6 +238,7 @@ private fun PendingScreenPreview() {
     ZiineTheme {
         PendingScreen(
             modifier = Modifier.background(MaterialTheme.colorScheme.background),
-            activityFinishAction = {})
+            activityFinishAction = {},
+        )
     }
 }
