01d149b1278e7f15f9428f9920d3b446013979b6
weakPig <chang5hustle@gmail.com>
2025-02-15 12:10:30 +0900

    [feat] 화면 구성용 UI 모델 값 추가 (#35)

--- presentation/src/main/kotlin/com/nexters/ziine/android/presentation/magazine/model/UiMagazine.kt
+++ presentation/src/main/kotlin/com/nexters/ziine/android/presentation/magazine/model/UiMagazine.kt
@@ -3,6 +3,7 @@ package com.nexters.ziine.android.presentation.magazine.model
 import kotlinx.collections.immutable.ImmutableList
 
 data class UiMagazine(
+    val magazineId: Int,
     val title: String,
     val subTitle: String,
     val keyWords: ImmutableList<String>,
