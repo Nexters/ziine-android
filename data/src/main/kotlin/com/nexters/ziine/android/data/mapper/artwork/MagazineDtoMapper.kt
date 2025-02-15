7d9aa8f6cbb562c4e354930765bf86f4eac318b6
weakPig <chang5hustle@gmail.com>
2025-02-15 21:47:12 +0900

    [feat] 서비스 및 DTO 작성 (#35)

---
+++ data/src/main/kotlin/com/nexters/ziine/android/data/mapper/artwork/MagazineDtoMapper.kt
@@ -0,0 +1,20 @@
+package com.nexters.ziine.android.data.mapper.artwork
+
+import com.nexters.ziine.android.data.httpClient.dto.response.MagazineResponse
+import com.nexters.ziine.android.domain.entity.Magazine
+import com.nexters.ziine.android.domain.entity.Magazines
+
+fun MagazineResponse.toMagazines(): Magazines =
+    Magazines(
+        this.magazines.map {
+            Magazine(
+                id = it.id,
+                title = it.title,
+                subTitle = it.summary,
+                keyWords = it.keywords,
+                thumbnailUrl = it.thumbnailImageUrl,
+                backgroundColor = it.backgroundColor,
+            )
+        },
+    )
+
