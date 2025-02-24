95d935be70295f1c857c1b417c03bf6f2a608403
easyhooon <mraz3068@gmail.com>
2025-02-25 07:41:36 +0900

    [refactor] TabController 로직 개선 (#54)
    Magazine Detail 진입시 Magazine 탭이 선택 되어있도록

--- presentation/src/main/kotlin/com/nexters/ziine/android/presentation/navigation/TabController.kt
+++ presentation/src/main/kotlin/com/nexters/ziine/android/presentation/navigation/TabController.kt
@@ -1,6 +1,7 @@
 package com.nexters.ziine.android.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
@@ -12,9 +13,26 @@ class TabController(
     private val currentDestination: NavDestination?
         @Composable get() = navController.currentBackStackEntryAsState().value?.destination
 
+    private var lastSelectedTab = mutableStateOf(MainTab.ARTWORKS)
+
     val currentTab: MainTab?
-        @Composable get() = MainTab.find { tab ->
-            currentDestination?.hasRoute(tab::class) == true
+        @Composable get() {
+            val tabFromDestination = MainTab.find { tab ->
+                currentDestination?.hasRoute(tab::class) == true
+            }
+
+            // 메인 탭 화면에 있을 때 마지막 선택된 탭 업데이트
+            if (tabFromDestination != null) {
+                lastSelectedTab.value = tabFromDestination
+            }
+
+            // Magazine 상세 화면에서는 Magazine 탭을 선택된 상태로 유지
+            if (tabFromDestination == null && currentDestination?.hasRoute<Route.MagazineDetail>() == true) {
+                return MainTab.MAGAZINE
+            }
+
+            // 현재 탭 또는 마지막 선택된 탭 반환
+            return tabFromDestination ?: lastSelectedTab.value
         }
 
     @Composable
